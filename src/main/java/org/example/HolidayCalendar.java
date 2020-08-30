4fd17e59c5576c017d6b7b0206781f27d5df229c
Pablo Lucero <plucero87@gmail.com>
2020-08-30 05:16:21 -0300

    add test04 and make it pass making dayOfWeekHoliday a list

--- src/main/java/org/example/HolidayCalendar.java
+++ src/main/java/org/example/HolidayCalendar.java
@@ -2,16 +2,18 @@ package org.example;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HolidayCalendar {
 
-    private DayOfWeek dayOfWeekHoliday;
+    private final List<DayOfWeek> dayOfWeekHoliday = new ArrayList<>();
 
     public boolean isHoliday(LocalDate aDate) {
-        return aDate.getDayOfWeek().equals(dayOfWeekHoliday);
+        return dayOfWeekHoliday.contains(aDate.getDayOfWeek());
     }
 
     public void makeDateOfWeekHoliday(DayOfWeek dayOfWeek) {
-        dayOfWeekHoliday = dayOfWeek;
+        dayOfWeekHoliday.add(dayOfWeek);
     }
 }
--- src/test/java/org/example/HolidayCalendarTest.java
+++ src/test/java/org/example/HolidayCalendarTest.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HolidayCalendarTest {
 
@@ -33,4 +33,14 @@ class HolidayCalendarTest {
         assertTrue(holidayCalendar.isHoliday(aSunday));
     }
 
+    @Test
+    void test04() {
+        LocalDate aSaturday = LocalDate.of(2014, 3, 1);
+        LocalDate aSunday = LocalDate.of(2014, 3, 2);
+        HolidayCalendar holidayCalendar = new HolidayCalendar();
+        holidayCalendar.makeDateOfWeekHoliday(DayOfWeek.SATURDAY);
+        holidayCalendar.makeDateOfWeekHoliday(DayOfWeek.SUNDAY);
+        assertTrue(holidayCalendar.isHoliday(aSaturday));
+        assertTrue(holidayCalendar.isHoliday(aSunday));
+    }
 }
