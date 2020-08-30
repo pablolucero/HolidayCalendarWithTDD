6d73c586f07e3a052f09b56437a8a29de7b3cea7
Pablo Lucero <plucero87@gmail.com>
2020-08-30 03:13:46 -0300

    make test02 pass

--- src/main/java/org/example/HolidayCalendar.java
+++ src/main/java/org/example/HolidayCalendar.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class HolidayCalendar {
 
     public boolean isHoliday(LocalDate aDate) {
-        return true;
+        return aDate.getDayOfWeek().equals(DayOfWeek.SATURDAY);
     }
 }
